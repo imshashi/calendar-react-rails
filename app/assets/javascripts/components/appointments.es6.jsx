561f12f8a7aea6504dd575fc711455503708d4b5
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-28 23:58:33 +0530

    first react component in rails app

---
+++ app/assets/javascripts/components/appointments.es6.jsx
@@ -0,0 +1,12 @@
+class Appointments extends React.Component {
+  render () {
+    return (
+      <h1> Calendar - React Rails </h1>
+    );
+  }
+}
+
+Appointments.propTypes = {
+  label: PropTypes.string
+};
+
