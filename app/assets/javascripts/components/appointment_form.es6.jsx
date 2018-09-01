1db35f732187b97eee6f3e8c2dda316c8912197d
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-09-01 23:53:24 +0530

    handling user form input using state

--- app/assets/javascripts/components/appointment_form.es6.jsx
+++ app/assets/javascripts/components/appointment_form.es6.jsx
@@ -1,14 +1,35 @@
 class AppointmentForm extends React.Component {
-  render () {
+  handleChange(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  }
+
+  render() {
     return (
       <div>
         <h3> Make a new appointment </h3>
         <form>
-          <input name="title" placeholder="Appointment Title" />
-          <input name="apt_time" placeholder="Date and Time" />
+          <input
+            name="title"
+            placeholder="Appointment Title"
+            value={ this.props.input_title }
+            onChange={ this.handleChange }
+          />
+          <input
+            name="apt_time"
+            placeholder="Date and Time"
+            value={ this.props.input_apt_time }
+            onChange={ this.handleChange }
+          />
           <input type="submit" value="Make Appointment" />
         </form>
       </div>
     );
   }
 }
+
+// AppointmentForm.propTypes = {
+//   onUserInput: PropTypes.func.isRequired
+// };
--- app/assets/javascripts/components/appointments.es6.jsx
+++ app/assets/javascripts/components/appointments.es6.jsx
@@ -1,9 +1,28 @@
 class Appointments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: this.props.appointments,
+      input_title: '',
+      input_apt_time: ''
+    }
+  }
+
+  handleUserInput(obj) {
+    this.setState(obj);
+  }
+
   render () {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={ this.props.appointments } />
+        <AppointmentForm
+          input_title={ this.state.input_title }
+          input_apt_time={ this.state.input_apt_time }
+          onUserInput={ this.state.handleUserInput }
+        />
+        <AppointmentsList
+          appointments={ this.state.appointments }
+        />
       </div>
     );
   }
